41b44693eb0ae3cfffa01b59d2b1a01fd3997811
mtndewaznotradley-ui <mtndewaznotradley@gmail.com>
2026-04-19 19:21:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Server is running 🚀");
-});
-
 app.post("/api/order", async (req, res) => {
     const { email } = req.body;
 
